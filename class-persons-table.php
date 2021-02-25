28d4e4bc88db22b503987ad07ff3252fcd789f0c
ari889 <arijitbanarjee889@gmail.com>
2021-02-25 12:59:10 +0600

    Wordpress datatable plugin

---
+++ class-persons-table.php
@@ -0,0 +1,87 @@
+<?php
+
+if (!class_exists('WP_List_Table')) {
+    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+}
+
+class Persons_Table extends WP_List_Table{
+
+    private $_items;
+
+    function __construct($args = array())
+    {
+        parent::__construct($args);
+    }
+
+    function set_data($data){
+        $this -> _items = $data;
+    }
+
+    function get_columns(){
+        return [
+            'cb' => '<input type="checkbox">',
+            'name' => __('Name', 'tabledata'),
+            'sex' => __('Gender', 'tabledata'),
+            'email' => __('Email', 'tabledata'),
+            'age' => __('Age', 'tabledata'),
+        ];
+    }
+
+    function get_sortable_columns()
+    {
+        return [
+            'age' => ['age', true],
+            'name' => ['name', true]
+        ];
+    }
+
+    function column_cb($item)
+    {
+        return "<input type='checkbox' value='{$item['id']}'/>";
+    }
+
+    function column_email($item){
+        return "<strong>{$item['email']}</strong>";
+    }
+
+    function column_age($item){
+        return "<strong>{$item['age']}</strong>";
+    }
+
+    function extra_tablenav($which)
+    {
+        if('top' == $which):
+        ?>
+        <div class="actions alignleft">
+            <select name="filter_s" id="filter_s">
+                <option value="all">All</option>
+                <option value="M">Male</option>
+                <option value="F">Female</option>
+            </select>
+            <?php submit_button(__('Filter', 'tabledata'), 'primary', 'submit', false); ?>
+        </div>
+    <?php
+        endif;
+    }
+
+    function prepare_items()
+    {
+        $paged = $_REQUEST['paged']??1;
+        $per_page = 4;
+        $total_items = count($this->_items);
+        $this->_column_headers = array($this->get_columns(), array(), $this->get_sortable_columns());
+        $data_chunks = array_chunk($this->_items, $per_page);
+        $this->items = $data_chunks[$paged-1];
+        $this -> set_pagination_args([
+            'total_items' => $total_items,
+            'per_page' => $per_page,
+            'total_pages' => ceil($total_items / $per_page)
+        ]);
+    }
+
+    function column_default($item, $column_name)
+    {
+        return $item[$column_name];
+    }
+
+}
---
+++ dataset.php
@@ -0,0 +1,43 @@
+<?php
+
+
+$data = [
+  [
+      'id' => 1,
+      'name' => 'Jhon doe',
+      'email' => 'jhon@gmail.com',
+      'age' => 24,
+      'sex' => 'M'
+  ],[
+        'id' => 2,
+        'name' => 'Jhon doe',
+        'email' => 'jhon@doe.com',
+        'age' => 25,
+        'sex' => 'F'
+    ],[
+        'id' => 3,
+        'name' => 'Jimmy doe',
+        'email' => 'jimmy@gmail.com',
+        'age' => 26,
+        'sex' => 'M'
+    ],[
+        'id' => 4,
+        'name' => 'Jessy doe',
+        'email' => 'jessy@gmail.com',
+        'age' => 29,
+        'sex' => 'M'
+    ],[
+        'id' => 5,
+        'name' => 'Jack doe',
+        'email' => 'Jack@gmail.com',
+        'age' => 27,
+        'sex' => 'F'
+    ],
+    [
+        'id' => 6,
+        'name' => 'Json doe',
+        'email' => 'json@gmail.com',
+        'age' => 27,
+        'sex' => 'F'
+    ],
+];
